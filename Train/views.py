76e7188b62c7a9c787e7a5e12a569f2c9c613d05
YunJunChoi <dbswnschl@gmail.com>
2017-11-13 18:01:42 +0900

    change method : file reading -> data reading

--- Train/views.py
+++ Train/views.py
@@ -7,7 +7,37 @@ import re
 import random
 from django.http import HttpResponseRedirect
 from .models import Trees
+from PIL import Image
+import io
+import base64
+from matplotlib import pyplot as plt
+import numpy as np
 def index(request):
+    trees = np.load('tree.npy')
+    print(trees.size)
+    for i in range(1):
+        print(trees[i])
+        # trees[i] = np.reshape(trees[i],(28,28))
+        img = np.reshape(trees[i],(28,28))
+        print(np.shape(img))
+        im = Image.fromarray(img)
+        image = im.crop()
+        bytearr = io.BytesIO()
+        image.save(bytearr,format="PNG")
+        bytearr = bytearr.getvalue()
+        encodebyte = base64.b64encode(bytearr)
+        str = encodebyte.decode('ascii')
+        print(str)
+        im.show()
+        print(np.size(trees))
+        print(len(trees))
+        return HttpResponse("""
+        <html>
+            <img src='data:image/png;base64,%s' style='width:280px'/>
+        
+        </html>
+        
+        """%str)
 
     return HttpResponse("HI")
     pass
@@ -23,24 +53,49 @@ def treeTrain(request):
             questions.append(opt)
             pass
         print(trees)
-        filenames = os.listdir(os.path.join(os.getcwd(),'static/images'))
+
+        treearr = np.load('tree.npy')
+        filecounts = len(treearr)
         for i in Trees.objects.all():
             while True:
-                ran = random.randrange(0,len(filenames))
+                ran = random.randrange(0,filecounts)
                 if ran == int(i.treeID)-1:
                     continue
                 else:
-                    print(ran)
-                    print("==")
-                    print(int(i.treeID)-1)
+                    print("random int : %s"%ran)
                     break
-        filename = filenames[ran]
+                    pass
+                pass
+
+        img = np.reshape(treearr[ran],(28,28))
+        imarr = Image.fromarray(img)
+        imcrop = imarr.crop()
+        btio = io.BytesIO()
+        imcrop.save(btio,format='PNG')
+        btio = btio.getvalue()
+        encodebyte = base64.b64encode(btio)
+        strPNG = encodebyte.decode('ascii')
+
+
+        # filenames = os.listdir(os.path.join(os.getcwd(),'static/images'))
+        # for i in Trees.objects.all():
+        #     while True:
+        #         ran = random.randrange(0,len(filenames))
+        #         if ran == int(i.treeID)-1:
+        #             continue
+        #         else:
+        #             print(ran)
+        #             print("==")
+        #             print(int(i.treeID)-1)
+        #             break
+        # filename = filenames[ran]
 
         quest = []
         for i in questions:
             quest.append(re.sub("\d","",i))
         print(quest)
-        return render(request, 'index.html', {'trees':trees,'filename':filename,'quest':quest, 'tq' : zip(trees,quest),'treenum' :filename.split('.')[0].split('tree')[1]})
+        # return render(request, 'index.html', {'trees':trees,'filename':filename,'quest':quest, 'tq' : zip(trees,quest),'treenum' :filename.split('.')[0].split('tree')[1]})
+        return render(request,'index.html',{'trees':trees,'quest':quest,'tq':zip(trees,quest),'treenum':ran,'treedata':strPNG,'maxnum':len(treearr)})
     else:
         return HttpResponse("Please GET request..")
     pass
@@ -78,6 +133,5 @@ def getTrain(request):
     )
     query.save()
     return HttpResponseRedirect('tree')
-
     pass
 # Create your views here.
